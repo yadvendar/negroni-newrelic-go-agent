9af1582c88ee544490a3d3b8912f053d438df83d
Yadvendar Champawat <yadvendar@testbook.com>
2016-07-12 15:25:05 +0530

    negroni middleware for go agent

---
+++ newrelic.go
@@ -0,0 +1,31 @@
+package negroninewrelic
+
+import (
+	"net/http"
+
+	"github.com/newrelic/go-agent"
+	"github.com/newrelic/go-agent/api"
+)
+
+type Newrelic struct {
+	Application *newrelic.Application
+	Transaction *newrelic.Transaction
+}
+
+func NewConfig(applicationName string, licenseKey string) api.Config {
+	return newrelic.NewConfig(applicationName, licenseKey)
+}
+func New(config api.Config) (*Newrelic, error) {
+	app, err := newrelic.NewApplication(config)
+	return &Newrelic{Application: &app}, err
+}
+
+func (n *Newrelic) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	(*n.Transaction) = (*n.Application).StartTransaction(r.URL.Path, rw, r)
+	defer (*n.Transaction).End()
+
+	// Use if required
+	//	(*n.Transaction).AddAttribute("query", r.URL.RawQuery)
+
+	next(rw, r)
+}
